816752ea0e8f4f13a14df47049d8a26b514007ac
hoodmanb <joshuadebravo@gmail.com>
2024-08-01 22:55:43 +0100

    100 updated

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,7 +6,8 @@ from typing import Sequence, Any, Union
 
 # The types of the elements of the input are not known
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
-    """Returns the first element of the sequence if it exists, otherwise None."""
+    """Returns the first element of the sequence if it exists
+    otherwise None."""
     if lst:
         return lst[0]
     else:
