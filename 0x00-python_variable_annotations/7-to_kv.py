77db98238c782666dd5e8d752aefb0a362a22667
hoodmanb <joshuadebravo@gmail.com>
2024-08-01 14:02:23 +0100

    7 uptated

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""function to_kv that takes a string k and an int OR float as
-arguments"""
+"""
+function to_kv that takes a string k and an int OR float as
+arguments
+"""
 from typing import Union, Tuple
 
 
