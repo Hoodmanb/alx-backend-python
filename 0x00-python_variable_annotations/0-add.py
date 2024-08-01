8c8366c58b3546ae652d586eb8c0c2eed0243442
hoodmanb <joshuadebravo@gmail.com>
2024-08-01 12:12:59 +0100

    added

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" functiom to atonate float numbers and return a floating num"""
+
+
+def add(a: float, b: float) -> float:
+    """ returns a floating point number"""
+    return a + b
