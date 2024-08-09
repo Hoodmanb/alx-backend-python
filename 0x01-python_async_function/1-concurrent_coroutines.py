743bbb300792a366e5748af742ad7a0707e788a3
hoodmanb <joshuadebravo@gmail.com>
2024-08-09 23:32:48 +0100

    added a dir

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,12 +4,12 @@ import asyncio
 from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ascending order without using sort() because of concurrency."""
     total = await asyncio.gather(
-        *[wait_random(max_delay) for x in range(n)]
+        *[task_wait_random(max_delay) for x in range(n)]
     )
     return sorted(total)
